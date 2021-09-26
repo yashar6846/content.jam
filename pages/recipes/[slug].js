26189b9e109298252393d584e1df073bb1fe4a52
yashar6846 <yasharnajafi6846@gmail.com>
2021-09-26 12:24:50 +0300

    first commit

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,62 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "recipe",
+  });
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+  return {
+    props: { recipe: items[0] },
+  };
+}
+export default function RecipeDetails({ recipe }) {
+  const { featuredImage, title, cookingTime, ingredients, method } =
+    recipe.fields;
+
   return (
     <div>
-      Recipe Details
+      <div className="banner">
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{title}</h2>
+      </div>
+      <div className="info">
+        <p>Take about {cookingTime} mins to cook</p>
+        <h3>Ingredients</h3>
+
+        {ingredients.map((ing) => (
+          <span key={ing}>{ing}</span>
+        ))}
+      </div>
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
     </div>
-  )
+  );
 }
